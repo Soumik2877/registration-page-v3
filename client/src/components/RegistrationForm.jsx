c4857a0ae6495f170ddf221079d4e53f4156a2b0
SOUMIK CHATTTERJEE <soumikchattterjee@SOUMIKs-MacBook-Air-2.local>
2025-07-16 12:45:21 +0530

    Initial commit

---
+++ client/src/components/RegistrationForm.jsx
@@ -0,0 +1,93 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import './RegistrationForm.css';
+
+
+function RegistrationForm() {
+  const [form, setForm] = useState({
+    name: '',
+    class: '',
+    section: '',
+    phone: '',
+    school: '',
+    otherSchoolName: ''
+  });
+  const [loading, setLoading] = useState(false);
+  const [message, setMessage] = useState('');
+  const showOtherSchool = form.school === 'Other';
+
+  const handleChange = e => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    setLoading(true);
+    setMessage('');
+    const data = {
+      ...form,
+      school: form.school === 'Other' ? form.otherSchoolName : form.school
+    };
+
+    try {
+      const res = await axios.post('http://127.0.0.1:5500/register', data);
+      setMessage(res.data || 'Submitted successfully');
+    }catch (err) {
+        setMessage('Submission failed');
+      }
+      finally{
+        setLoading(false);
+      setForm({
+        name: '',
+        class: '',
+        section: '',
+        phone: '',
+        school: '',
+        otherSchoolName: ''
+      });
+    } 
+  };
+
+  return (
+    <div style={{ maxWidth: '500px', margin: 'auto' }}>
+        <div className="form-container">
+            <div className="logos">
+            <img src="/images/logo1.png" alt="Logo" className="logo1"/>
+            <img src="/images/logo2.png" alt="Logo" className="logo2"/>
+            </div>
+      <h2>Student Registration</h2>
+      <form onSubmit={handleSubmit}>
+        <input name="name" placeholder="Student Name" value={form.name.toUpperCase()} onChange={handleChange} required />
+        <input name="class" placeholder="Class" value={form.class.toUpperCase()} onChange={handleChange} required />
+        <input name="section" placeholder="Section" value={form.section.toUpperCase()} onChange={handleChange} required />
+        <input name="phone" placeholder="Phone Number" value={form.phone} onChange={handleChange} required />
+
+        <select name="school" value={form.school} onChange={handleChange} required>
+          <option value="">-- Select School --</option>
+          <option value="MBA">MBA</option>
+          <option value="SDIS">SDIS</option>
+          <option value="Other">Other</option>
+        </select>
+
+        {showOtherSchool && (
+          <input name="otherSchoolName" placeholder="Other School Name" value={form.otherSchoolName} onChange={handleChange} required />
+        )}
+
+        <button type="submit">Submit</button>
+        </form>
+
+        {loading ? (
+  <div className="spinner"></div>
+    ) : 
+    (
+        message &&( 
+    <div className='msg' >{message}</div> )
+)}
+      
+      </div>
+     
+    </div>
+  );
+}
+
+export default RegistrationForm;
---
+++ server/index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
+
+const app = express();
+const PORT = 5500;
+
+// ✅ Allow requests from frontend
+const corsOptions ={
+    origin:'http://localhost:3001', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
+app.use(express.json());
+
+// 🔄 Replace with your actual Google Script URL
+//deployment without color code
+const GOOGLE_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbySACAmYnRu9qLk84dumGxQVL-33TuG2dmVdP4Dss6U43bRGmYA84YWNDLk0ylKgNaK/exec';
+//deployment with color code
+const GOOGLE_SCRIPT_URL2 = 'https://script.google.com/macros/s/AKfycbxJZt9GwYpyTybArphtnMQ32sgqf9nuV0316JzarNZ9SJVB9TW5b0O6xURHyBB3H1o/exec';
+//last row issue solved
+const GOOGLE_SCRIPT_URL3 = 'https://script.google.com/macros/s/AKfycbxkAjGAoNEcBWeYuF6fOHdalHJz32mN1SvEXi7QFK9CULS8YdlYWQuk3ZIwJL4F9rWo/exec';
+//section to uppercase on appscript v4
+const GOOGLE_SCRIPT_URL4 = 'https://script.google.com/macros/s/AKfycbzPzgX-Fb-a9YbCIg6YZ-wqja6A3788QLWBpLXPF4H6DGBh7NmtqRI5geQTXhx3Sc4/exec';
+//Final_v5
+const GOOGLE_SCRIPT_URL5 = 'https://script.google.com/macros/s/AKfycby9vS-UIYWa5Nd4RcMzRf91P80_jWMFQlX-E9FexwNnuAy703b6apn8NGNeKWy1NOEa/exec';
+app.post('/register', async (req, res) => {
+  try {
+    const response = await axios.post(GOOGLE_SCRIPT_URL5, req.body);
+    res.send(response.data);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Error submitting to Google Sheet');
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://127.0.0.1:${PORT}`);
+});
